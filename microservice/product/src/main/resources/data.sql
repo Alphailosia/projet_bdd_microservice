7fc06d00b3ec52a80a740020608713c4d127a544
Sabrina <sabrina.mercuri@etu.unice.fr>
2022-03-17 18:03:12 +0100

    début amélioration front

--- microservice/product/src/main/resources/data.sql
+++ microservice/product/src/main/resources/data.sql
@@ -1,10 +1,10 @@
 INSERT INTO product(id,name, description, illustration, price)
-values (0, 'Pop Dr Strange', ' (description) Pop Dr Strange #meilleursuperheros','https://cdn.idealo.com/folder/Product/201631/5/201631590/s4_produktbild_gross/funko-pop-marvel-what-if-doctor-strange-supreme.jpg',9.9),
-       (1, 'Produit1', 'description produit 1','https://images.squarespace-cdn.com/content/v1/576c0682f5e231d654cf661c/1528449391201-4OIJK4NQYFQTD8Y102W2/shopping-cart.png',9.9),
-       (2, 'Produit2', 'description produit 2','https://images.squarespace-cdn.com/content/v1/576c0682f5e231d654cf661c/1528449391201-4OIJK4NQYFQTD8Y102W2/shopping-cart.png',9.9),
-       (3, 'Produit3', 'description produit 3','https://images.squarespace-cdn.com/content/v1/576c0682f5e231d654cf661c/1528449391201-4OIJK4NQYFQTD8Y102W2/shopping-cart.png',9.9),
-       (4, 'Produit4', 'description produit 4','https://images.squarespace-cdn.com/content/v1/576c0682f5e231d654cf661c/1528449391201-4OIJK4NQYFQTD8Y102W2/shopping-cart.png',9.9),
-       (5, 'Produit5', 'description produit 5','https://images.squarespace-cdn.com/content/v1/576c0682f5e231d654cf661c/1528449391201-4OIJK4NQYFQTD8Y102W2/shopping-cart.png',9.9),
-       (6, 'Produit6', 'description produit 6','https://images.squarespace-cdn.com/content/v1/576c0682f5e231d654cf661c/1528449391201-4OIJK4NQYFQTD8Y102W2/shopping-cart.png',9.9),
-       (7, 'Produit7', 'description produit 7','https://images.squarespace-cdn.com/content/v1/576c0682f5e231d654cf661c/1528449391201-4OIJK4NQYFQTD8Y102W2/shopping-cart.png',9.9),
-       (8, 'Produit8', 'description produit 8','https://images.squarespace-cdn.com/content/v1/576c0682f5e231d654cf661c/1528449391201-4OIJK4NQYFQTD8Y102W2/shopping-cart.png',9.9);
+values (0, 'Pop Dr Strange', 'Pop Dr Strange - What if','https://cdn.idealo.com/folder/Product/201631/5/201631590/s4_produktbild_gross/funko-pop-marvel-what-if-doctor-strange-supreme.jpg',9.9),
+       (1, 'Pop Wanda', 'Pop Wanda - WandaVision','https://www.techinn.com/f/13795/137954183/funko-pop-marvel-wandavision-wanda-halloween.jpg',9.9),
+       (2, 'Pop Iron Man', 'Pop Iron Man - Avengers Endgame','https://www.123comparer.fr/photoProd/zoom/3135/iron-man-i-am-iron-man-avengers-endgame-figurine-funko-pop-exclu-174717170.jpg',9.9),
+       (3, 'Pop Captain Marvel', 'Pop Captain Marvel - Avengers Endgame','https://m.media-amazon.com/images/I/61oqUksjJmL._AC_SX466_.jpg',9.9),
+       (4, 'Pop Thor', 'Pop Thor - Avengers Endgame','https://m.media-amazon.com/images/I/61SfHHyJEJL._AC_SX466_.jpg',9.9),
+       (5, 'Pop Black Widow', 'Pop Black Widow - Avengers','https://m.media-amazon.com/images/I/619awT1fZKL._AC_SY606_.jpg',9.9),
+       (6, 'Pop Captain Carter', 'Pop Captain Carter - What if','https://m.media-amazon.com/images/I/51ErU1YKLuS._AC_SY450_.jpg',9.9),
+       (7, 'Pop Hawkeye', 'Pop Hawkeye - Avengers','https://m.media-amazon.com/images/I/71c4juUQJhL._AC_SY450_.jpg',9.9),
+       (8, 'Pop Captain America', 'Pop Captain America - Avengers Endgame','https://i.pinimg.com/originals/58/35/b8/5835b88cb31b6dbd1910532a3ceff86f.jpg',9.9);

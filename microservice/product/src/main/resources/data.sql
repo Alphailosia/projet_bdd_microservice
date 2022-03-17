c39bbb486ce4207f44a5e15eda4dddbc1e566268
Sabrina <sabrina.mercuri@etu.unice.fr>
2022-03-17 19:00:52 +0100

    suite affichage index

--- microservice/product/src/main/resources/data.sql
+++ microservice/product/src/main/resources/data.sql
@@ -1,10 +1,11 @@
 INSERT INTO product(id,name, description, illustration, price)
-values (0, 'Pop Dr Strange', 'Pop Dr Strange - What if','https://cdn.idealo.com/folder/Product/201631/5/201631590/s4_produktbild_gross/funko-pop-marvel-what-if-doctor-strange-supreme.jpg',9.9),
-       (1, 'Pop Wanda', 'Pop Wanda - WandaVision','https://www.techinn.com/f/13795/137954183/funko-pop-marvel-wandavision-wanda-halloween.jpg',9.9),
-       (2, 'Pop Iron Man', 'Pop Iron Man - Avengers Endgame','https://www.123comparer.fr/photoProd/zoom/3135/iron-man-i-am-iron-man-avengers-endgame-figurine-funko-pop-exclu-174717170.jpg',9.9),
-       (3, 'Pop Captain Marvel', 'Pop Captain Marvel - Avengers Endgame','https://m.media-amazon.com/images/I/61oqUksjJmL._AC_SX466_.jpg',9.9),
-       (4, 'Pop Thor', 'Pop Thor - Avengers Endgame','https://m.media-amazon.com/images/I/61SfHHyJEJL._AC_SX466_.jpg',9.9),
-       (5, 'Pop Black Widow', 'Pop Black Widow - Avengers','https://m.media-amazon.com/images/I/619awT1fZKL._AC_SY606_.jpg',9.9),
-       (6, 'Pop Captain Carter', 'Pop Captain Carter - What if','https://m.media-amazon.com/images/I/51ErU1YKLuS._AC_SY450_.jpg',9.9),
+values (0, 'Pop Dr Strange', 'Pop Dr Strange - What if','https://cdn.idealo.com/folder/Product/201631/5/201631590/s4_produktbild_gross/funko-pop-marvel-what-if-doctor-strange-supreme.jpg',11.9),
+       (1, 'Pop Wanda', 'Pop Wanda - WandaVision','https://www.techinn.com/f/13795/137954183/funko-pop-marvel-wandavision-wanda-halloween.jpg',15.3),
+       (2, 'Pop Iron Man', 'Pop Iron Man - Avengers Endgame','https://www.123comparer.fr/photoProd/zoom/3135/iron-man-i-am-iron-man-avengers-endgame-figurine-funko-pop-exclu-174717170.jpg',16.9),
+       (3, 'Pop Captain Marvel', 'Pop Captain Marvel - Avengers Endgame','https://m.media-amazon.com/images/I/61oqUksjJmL._AC_SX466_.jpg',12.2),
+       (4, 'Pop Thor', 'Pop Thor - Avengers Endgame','https://m.media-amazon.com/images/I/61SfHHyJEJL._AC_SX466_.jpg',10),
+       (5, 'Pop Black Widow', 'Pop Black Widow - Avengers','https://m.media-amazon.com/images/I/619awT1fZKL._AC_SY606_.jpg',10.5),
+       (6, 'Pop Captain Carter', 'Pop Captain Carter - What if','https://m.media-amazon.com/images/I/51ErU1YKLuS._AC_SY450_.jpg',11.8),
        (7, 'Pop Hawkeye', 'Pop Hawkeye - Avengers','https://m.media-amazon.com/images/I/71c4juUQJhL._AC_SY450_.jpg',9.9),
-       (8, 'Pop Captain America', 'Pop Captain America - Avengers Endgame','https://i.pinimg.com/originals/58/35/b8/5835b88cb31b6dbd1910532a3ceff86f.jpg',9.9);
+       (8, 'Pop Captain America', 'Pop Captain America - Avengers Endgame','https://i.pinimg.com/originals/58/35/b8/5835b88cb31b6dbd1910532a3ceff86f.jpg',16.9),
+       (9, 'Pop Bucky', 'Pop Bucky - Winter Soldier','https://m.media-amazon.com/images/I/61iA1yhz+7L._AC_SL1500_.jpg',15.7);
